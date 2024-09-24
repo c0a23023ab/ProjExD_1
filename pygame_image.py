26bc1d835c2f690215ed39ccd5c81c881882c613
C0A23023 <c0a23023ab@edu.teu.ac.jp>
2024-09-24 16:35:14 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -26,14 +26,16 @@ def main():
         screen.blit(bg_img, [x + 3200, 0])
         screen.blit(bga_img, [x + 4800, 0])
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kt_rct.move_ip((0, -1))
-        elif key_lst[pg.K_DOWN]:
-            kt_rct.move_ip((0, 1))
-        elif key_lst[pg.K_RIGHT]:
-            kt_rct.move_ip((1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((2, 0))
         elif key_lst[pg.K_LEFT]:
             kt_rct.move_ip((-1, 0))
+        else:
+            kt_rct.move_ip((-1, 0))
+            if key_lst[pg.K_UP]:
+                kt_rct.move_ip((0, -1))
+            elif key_lst[pg.K_DOWN]:
+                kt_rct.move_ip((0, 1))
         screen.blit(kt_img,kt_rct)
         pg.display.update()
         tmr += 1        
