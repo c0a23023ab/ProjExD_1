a65dd984f12de5178c0bb86097e18e2e3cd10752
C0A23023 <c0a23023ab@edu.teu.ac.jp>
2024-09-24 16:18:43 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -15,6 +15,8 @@ def main():
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img,True,False)
     bga_img = pg.transform.flip(bg_img,True,False)
+    kt_rct = kt_img.get_rect()
+    kt_rct.center = 300,200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -23,11 +25,20 @@ def main():
         screen.blit(bga_img, [x + 1600, 0])
         screen.blit(bg_img, [x + 3200, 0])
         screen.blit(bga_img, [x + 4800, 0])
-        screen.blit(kt_img,[300,200])
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0, 1))
+        elif key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((1, 0))
+        elif key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1, 0))
+        screen.blit(kt_img,kt_rct)
         pg.display.update()
         tmr += 1        
         x = -(tmr%3200)
-        clock.tick(600)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
